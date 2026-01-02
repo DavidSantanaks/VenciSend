7715324de03f19ed0fa021d762050059b566dfa3
davidsantanaks <davidfjobs@gmail.com>
2026-01-02 11:44:58 -0300

    feat: base do projeto

---
+++ src/main/java/com/br/projetoVenciSend/ProjetoVenciSendApplication.java
@@ -0,0 +1,13 @@
+package com.br.projetoVenciSend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVenciSendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVenciSendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/projetoVenciSend/ProjetoVenciSendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projetoVenciSend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVenciSendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
