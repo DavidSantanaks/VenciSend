e1a109dacf62c2d17d5d5b2001cd6ec0fc50ae71
davidsantanaks <davidfjobs@gmail.com>
2026-01-03 00:31:26 -0300

    feat: campo ativo no dto

--- src/main/java/com/br/projetoVenciSend/pessoa/Record/PessoasCriarDTO.java
+++ src/main/java/com/br/projetoVenciSend/pessoa/Record/PessoasCriarDTO.java
@@ -1,4 +1,4 @@
 package com.br.projetoVenciSend.pessoa.Record;
 
-public record PessoasCriarDTO(Long id, String nome, String senha) {
+public record PessoasCriarDTO(Long id, String nome, String senha, Boolean ativo) {
 }
